dc5ccedc7ea5f3251da5891b9f1010ea438cc2de
Mohamed Diallo <diamhd100@gmail.com>
2024-09-17 12:45:13 +0000

    autre modification

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1!_y^r=_zmt+41gocpp#-m-h8zst_xk^sfe!ke^q#%r72y!va7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mohamed100.pythonanywhere.com','127.0.0.1']
 
